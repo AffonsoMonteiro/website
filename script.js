5a8d7061c93d5828b58c2583b5a56f3830008d85
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2020-08-19 20:08:20 -0300

    add js e finalizado o site

---
+++ script.js
@@ -0,0 +1,6 @@
+function toglleMenu() {
+    var menuToggle = document.querySelector('.menuToggle')
+    var navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
